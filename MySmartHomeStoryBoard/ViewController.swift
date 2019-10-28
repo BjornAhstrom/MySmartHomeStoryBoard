0f348ed2834afefaac93cadbafd6677dc6150209
Björn Åhström <ahstrom_81@hotmail.com>
2019-10-28 18:16:07 +0100

    Initial Commit

---
+++ MySmartHomeStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySmartHomeStoryBoard
+//
+//  Created by Björn Åhström on 2019-10-28.
+//  Copyright © 2019 Björn Åhström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
